b972aa45fae5f29be539a5e51309a0de3fc50cd4
gintariukai <gintariukai@hotmail.com>
2023-03-13 21:55:30 +0200

    Added Await, defer, update shop file, delete useState and useEffect, add Suspense and create async function, add useAsyncValue

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,42 +1,58 @@
-import {useState, useEffect} from "react";
-import {Link, useSearchParams} from "react-router-dom";
+import {Await, defer, Link, useLoaderData, useSearchParams} from "react-router-dom";
 import ItemsFilter from "../components/ItemsFilter";
+import {Suspense} from "react";
 
 const Shop = () => {
-    const [shop, setShop] = useState([]);
-    // console.log(useLocation());
+    const {posts} = useLoaderData();
 
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const shopQuery = searchParams.get("shop") || "";
-    const latest = searchParams.has("latest");
+    const itemQuery = searchParams.get('post') || '';
+    const latest = searchParams.has('latest');
 
     const startsForm = latest ? 80 : 1;
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts`)
-            .then(res => res.json())
-            .then(data => setShop(data))
-    }, []);
 
     return (
         <div>
             <h1>Our product</h1>
 
-            <ItemsFilter shopQuery={shopQuery} latest={latest} setSearchParams={setSearchParams}/>
+            <ItemsFilter itemQuery={itemQuery} latest={latest} setSearchParams={setSearchParams}/>
 
-            <Link to="/shop/create">Add new product</Link>
-            {
-                shop.filter(
-                    post => post.title.includes(shopQuery) && post.id >= startsForm
-                ).map(post => (
-                    <Link key={post.id} to={`/posts/${post.id}`} >
-                        <li>{post.title}</li>
-                    </Link>
-                ))
-            }
+            <Link to="/posts/new" className="create" >Add new product</Link>
+
+            <Suspense fallback={<h2>Loading...</h2>}>
+                <Await resolve={posts}>
+                    {
+                        (resolvedPosts) => (<>
+                            {
+                                resolvedPosts.filter(
+                                    post => post.title.includes(itemQuery) && post.id >= startsForm
+                                ).map(post => (
+                                    <Link key={post.id} to={`/posts/${post.id}`} >
+                                        <li>{post.title}</li>
+                                    </Link>
+                                ))
+                            }
+                        </>)
+                    }
+                </Await>
+            </Suspense>
         </div>
     );
 }
 
+async function getItems() {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+    return res.json()
+}
+
+const shopLoader = async () => {
+
+    return defer({
+        posts: getItems()
+    })
+}
+
 export default Shop;
+export {shopLoader};
--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,32 +1,75 @@
-import {useParams, Link, useNavigate} from "react-router-dom";
-import {useState, useEffect} from "react";
+import {Await, defer, Link, useAsyncValue, useLoaderData, useNavigate} from "react-router-dom";
+import {Suspense} from "react";
 
+const Post = () => {
+    const post = useAsyncValue()
+
+    return (
+        <>
+        <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </>
+    )
+}
+
+const Comments = () => {
+    const comments = useAsyncValue();
+
+    return (
+        <div>
+            <h2>Comments</h2>
+            {comments.map(comment => (
+                <>
+                <h3>{comment.email}</h3>
+                <h4>{comment.name}</h4>
+                <p>{comment.body}</p>
+                </>
+            ))}
+        </div>
+    )
+}
 
 const Single = () => {
-    const {id} = useParams();
+    const {post, id, comments} = useLoaderData()
     const navigate = useNavigate();
-    const [shop, setShop] = useState(null);
 
     const goBack = () => navigate(-1);
-
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then(res => res.json())
-            .then(data => setShop(data))
-    }, [id]);
 
     return (
         <div>
             <button onClick={goBack}>Go back</button>
-            {shop && (
-                <>
-                    <h1>{shop.title}</h1>
-                    <p>{shop.body}</p>
-                    <Link to={`/posts/${id}`}>Edit the shop</Link>
-                </>
-            )}
+            <Suspense fallback={<h2>Item is loading...</h2>}>
+                <Await resolve={post}>
+                    <Post/>
+                </Await>
+            </Suspense>
+            <Suspense fallback={<h2>Comments is loading...</h2>}>
+                <Await resolve={comments}>
+                    <Comments/>
+                </Await>
+            </Suspense>
+            <Link to={`/posts/${id}/edit`}>Edit this Item</Link>
         </div>
-    );
+    )
+}
+
+async function getItemById(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+
+    return res.json()
+}
+
+async function getCommentsByItem(id) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+
+    return res.json()
+}
+
+const postLoader = async ({params}) => {
+    const id = params.id;
+
+    return defer({post: await getItemById(id), id, comments: getCommentsByItem(id)})
 }
 
 export default Single;
+export {postLoader};
