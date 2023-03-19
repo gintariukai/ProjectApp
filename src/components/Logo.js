1d1a69cb5827d79eba36ae646b2f65d3453f1336
gintariukai <gintariukai@hotmail.com>
2023-03-19 16:03:08 +0200

    Added logo components and image logo

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import logo from "../img/nomeda-icon.png";
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} className="logo" alt="logo"/>
+        </div>
+    )
+}
+
+export default Logo;
